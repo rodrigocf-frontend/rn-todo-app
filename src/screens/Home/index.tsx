bd9e60944f7a63feadab4a7b36ad97fffbc51f22
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2025-10-30 21:50:35 -0300

    feat: create home layout

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,23 +1,54 @@
 import {
-  CheckCircleIcon,
-  CircleIcon,
-  PlusCircleIcon,
-  TrashIcon,
-} from "phosphor-react-native";
-import { Button, Container } from "./styles";
+  Body,
+  Button,
+  Container,
+  CounterWrapper,
+  Header,
+  InputWrapper,
+  Logo,
+  TodoList,
+} from "./styles";
 import { Input } from "../../components/Input";
+import { useTheme } from "styled-components/native";
+import { ClipboardIcon, PlusCircleIcon } from "phosphor-react-native";
+import { Counter } from "../../components/Counter";
+import { useState } from "react";
+import { Task } from "../../components/Task";
+import { FlatList } from "react-native";
+import { EmptyTasks } from "../../components/EmptyTasks";
 
 export function Home() {
-  return (
-    <Container>
-      <CheckCircleIcon size={60} />
-      <CircleIcon size={60} />
-      <PlusCircleIcon size={60} />
-      <Button>
-        <TrashIcon size={60} />
-      </Button>
+  const theme = useTheme();
 
-      <Input placeholder="teste" />
-    </Container>
+  const [data, setData] = useState();
+
+  return (
+    <>
+      <Container>
+        <Header>
+          <Logo />
+
+          <InputWrapper>
+            <Input placeholder="Adicione uma nova tarefa" />
+            <Button>
+              <PlusCircleIcon size={16} color={theme.color.GRAY_100} />
+            </Button>
+          </InputWrapper>
+        </Header>
+        <Body>
+          <CounterWrapper>
+            d
+            <Counter title="Criadas" value={0} labelColor="BLUE_500" />
+            <Counter title="Concluídas" value={0} labelColor="PURPLE_900" />
+          </CounterWrapper>
+
+          <TodoList
+            data={[]}
+            renderItem={() => <Task isCompleted />}
+            ListEmptyComponent={<EmptyTasks />}
+          />
+        </Body>
+      </Container>
+    </>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,6 +1,53 @@
+import { Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import styled from "styled-components/native";
 
-export const Container = styled(SafeAreaView)``;
+export const Container = styled(SafeAreaView)`
+  flex: 1;
+`;
 
-export const Button = styled.TouchableOpacity``;
+export const Header = styled.View`
+  padding: 0 24px 0 24px;
+  background-color: ${({ theme }) => theme.color.GRAY_700};
+  flex: 1;
+  z-index: 1;
+`;
+
+export const Logo = styled(Image).attrs({
+  source: require("../../../assets/logo.png"),
+})`
+  margin: 24px auto 40px auto;
+`;
+
+export const InputWrapper = styled.View`
+  flex-direction: row;
+  gap: 4px;
+  align-items: center;
+`;
+export const Button = styled.TouchableOpacity.attrs({
+  activeOpacity: 1,
+})`
+  background-color: ${({ theme }) => theme.color.BLUE_900};
+  padding: 18px;
+  border-radius: 6px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Body = styled.View`
+  flex: 5.7;
+  background-color: ${({ theme }) => theme.color.GRAY_600};
+  padding: 55px 24px 0 24px;
+`;
+
+export const CounterWrapper = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const TodoList = styled.FlatList.attrs({
+  contentContainerStyle: {
+    rowGap: 5,
+    marginTop: 20,
+  },
+})``;
